ab04134cbdfd65db0bdc930679d1c1d2090758a9
userhagen <144329156+userhagen@users.noreply.github.com>
2023-09-10 12:27:27 +0200

    Add files via upload
    Little Python Snippet to extract necessary Keys & IDs from a preferences xml file

---
+++ xml3.py
@@ -0,0 +1,55 @@
+import xmltodict
+
+with open('./preferences.xml') as fd:
+    dictionary = xmltodict.parse(fd.read())
+
+inhalt = str(dictionary)
+
+einzel = inhalt.split('},{')
+
+for x in range(len(einzel)):
+    test = str(einzel[x])
+
+    start = test.find('"activeTime"')
+    if start > -1:
+        wois = test.find("virtual")
+        wois = wois -2 
+        gefunden = test[start:wois]
+        
+        zuordnung = gefunden.split(',')
+
+        device_name = "Device Name\t: "
+        local_key   = "Local Key  \t: "
+        dps         = "DPS        \t: "
+        device_id   = "Device ID  \t: "
+        data_teil  = ""
+        
+        for y in range(len(zuordnung)):
+            pos = zuordnung[y].find('name')
+            if pos > -1:
+                data_teil = zuordnung[y]
+                device_name += data_teil[7:]
+            
+            pos = zuordnung[y].find('localKey')
+            if pos > -1:
+                data_teil = zuordnung[y]
+                local_key += data_teil[11:]
+
+            pos = zuordnung[y].find('devId')
+            if pos > -1:
+                data_teil = zuordnung[y]
+                device_id += data_teil[8:]
+                    
+            pos = zuordnung[y].find('"1"')
+            if pos > -1:
+                data_teil = zuordnung[y]
+                data_teil += zuordnung[y+1]
+                data_teil += zuordnung[y+2]
+                dps += data_teil[6:]
+
+        print (device_name)
+        print (local_key)
+        print (device_id)
+        print (dps)
+        
+        print ("------------------------------------------------------------")
